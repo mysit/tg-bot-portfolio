cdbefb2647af7cfdcbf6623117d13bd043c86692
dmitry <noraner631@gmail.com>
2026-09-14 14:14:38 +0300

    Update database security system.

--- src/main/java/org/example/DatabaseManager.java
+++ src/main/java/org/example/DatabaseManager.java
@@ -8,9 +8,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class DatabaseManager {
-    public static final String DB_URL = "jdbc:mysql://bt5svlnsd8w9cvtv5njc-mysql.services.clever-cloud.com:3306/bt5svlnsd8w9cvtv5njc?useSSL=false&serverTimezone=UTC";
-    public static final String DB_User = "uyhgdwihlyqizep2";
-    public static final String DB_Password = "1RBGrViKsJnCeT1ONF9O";
+    public static final String DB_URL = System.getenv("DB_URL");
+    public static final String DB_User = System.getenv("DB_USER");
+    public static final String DB_Password = System.getenv("DB_PASSWORD");
 
     public static Map<String, String> getAllUsers() {
         // Используем LinkedHashMap, чтобы сохранить порядок вывода из базы
